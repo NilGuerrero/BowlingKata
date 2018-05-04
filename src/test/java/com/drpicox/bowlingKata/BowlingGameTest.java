161af1de7891b044bd07d67a1e531e8288c7e64e
NilGuerrero <guerrenil@gmail.com>
2018-05-04 08:48:04 +0200

    Slide 19

--- src/test/java/com/drpicox/bowlingKata/BowlingGameTest.java
+++ src/test/java/com/drpicox/bowlingKata/BowlingGameTest.java
@@ -2,7 +2,9 @@ package com.drpicox.bowlingKata;
 
 import org.junit.Test;
 
+import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertThat;
+
 
 public class BowlingGameTest {
     @Test
@@ -11,6 +13,7 @@ public class BowlingGameTest {
         for(int i=0; i<20; i++){
             g.roll(0);
         }
+        assertThat(g.score(), is(0));
     }
 
 
