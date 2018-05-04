ef7910737c33cca3c19958bf138c78acdba35c9d
NilGuerrero <guerrenil@gmail.com>
2018-05-04 08:51:47 +0200

    Slide 22

--- src/test/java/com/drpicox/bowlingKata/BowlingGameTest.java
+++ src/test/java/com/drpicox/bowlingKata/BowlingGameTest.java
@@ -8,12 +8,22 @@ import static org.junit.Assert.assertThat;
 
 public class BowlingGameTest {
     @Test
-    public void testGutterGame() throws Exception{
+    public void GutterGame() throws Exception{
         com.drpicox.bowlingKata.Game g=new com.drpicox.bowlingKata.Game();
         for(int i=0; i<20; i++){
             g.roll(0);
         }
         assertThat(g.score(), is(0));
+    }
+
+    @Test
+    public void testAllOnes() throws Exception{
+        //Test roll game
+        Game g=new Game();
+        for(int i=0;i<20;i++){
+            g.roll(1);
+        }
+        assertThat(g.score(), is(20));
     }
 
 
